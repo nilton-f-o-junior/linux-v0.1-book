c226dfdf62a936a65ee4d133c6e4d256fb063c8e
nilton-f-o-junior <nilton.f.o.junior@gmail.com>
2026-07-13 12:35:30 -0300

    refactor(docs): reorganiza estrutura de capítulos
    - Move os arquivos relacionados a sistema da pasta arquivos/ para sistema/.
    - Atualiza o SUMMARY.md para refletir a nova hierarquia de capítulos e nomes.
    - Remove arquivos obsoletos da estrutura antiga de arquivos/ para manter a organização consistente.
    - Melhora a navegação do livro ao consolidar conceitos de sistema.

--- linux-v0.1-book/book/searcher-09f2665d.js
+++ linux-v0.1-book/book/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-da940e3e.js',
+                path_to_root + 'searchindex-c9e3b045.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
