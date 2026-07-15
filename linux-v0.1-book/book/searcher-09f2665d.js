f1e00f815c1631fb9c1d874a7ae3638f848ea26a
nilton-f-o-junior <nilton.f.o.junior@gmail.com>
2026-07-14 23:06:27 -0300

    chore(livro): atualiza HTML gerado do build do livro
    - Regenera todas as páginas HTML do diretório book/ com base nas
      alterações feitas nos arquivos-fonte markdown
    - Atualiza referências a hashes de arquivos CSS e JS estáticos
      (general, chrome, book, searchindex) conforme nova compilação

--- linux-v0.1-book/book/searcher-09f2665d.js
+++ linux-v0.1-book/book/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-c9e3b045.js',
+                path_to_root + 'searchindex-00a83828.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
