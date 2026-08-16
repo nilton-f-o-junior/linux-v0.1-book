15748750ad474b190dcf50c232974afd94b6cca9
nilton.f.o.junior <nilton.f.o.junior@gmail.com>
2026-08-16 19:19:04 -0300

    chore(book): regera build do mdbook
    - Regenera o conteúdo estático em book/ a partir das fontes em src/
    - Atualiza os HTML de todas as páginas, índice, sumário e impressão
    - Incorpora a reformatação dos exemplos e o novo CSS customizado
    - Atualiza o searcher e remove o searchindex antigo, substituído
      pela versão regenerada

--- linux-v0.1-book/book/searcher-c2a407aa.js
+++ linux-v0.1-book/book/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-9f014f95.js',
+                path_to_root + 'searchindex-bf540dc1.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
